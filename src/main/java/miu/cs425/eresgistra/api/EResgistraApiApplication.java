ba6bd2e68655008f103acf56fb4323bed91b9ac1
Huy Dang <dinhhuy.dang@miu.edu>
2023-06-17 10:34:46 -0500

    initiate project

---
+++ src/main/java/miu/cs425/eresgistra/api/EResgistraApiApplication.java
@@ -0,0 +1,13 @@
+package miu.cs425.eresgistra.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EResgistraApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EResgistraApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/miu/cs425/eresgistra/api/EResgistraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package miu.cs425.eresgistra.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EResgistraApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
